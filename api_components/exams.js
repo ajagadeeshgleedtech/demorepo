feb16cbedeb0ac685b142332a2153d3800a1cf1f
Jagadeesh <ajagadeesh@gleedtech.com>
2017-05-06 15:20:46 +0530

    code update

---
+++ api_components/exams.js
@@ -0,0 +1,232 @@
+// flow
+var express = require("express");
+var config = require("../config.json");
+var bodyParser = require("body-parser");
+var api_key = "api-key-KJFSI4924R23RFSDFSD7F94";
+var mongo = require('mongodb').MongoClient;
+var autoIncrement = require("mongodb-autoincrement");
+var assert = require('assert');
+var port = process.env.PORT || 4005;
+var router = express.Router();
+var url = 'mongodb://' + config.dbhost + ':27017/s_erp_data';
+
+var cookieParser = require('cookie-parser');
+router.use(function(req, res, next) {
+    // do logging
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// Add Exams
+
+router.route('/exams/:subject_id/:exam_sch_id')
+    .post(function(req, res, next) {
+        var status = 1;
+        var subject_id = req.params.subject_id;
+        var exam_sch_id = req.params.exam_sch_id;
+        subjects = [];
+        var item = {
+            exam_paper_id: 'getauto',
+            subject_id: subject_id,
+            exam_sch_id: exam_sch_id,
+            exam_paper_title: req.body.exam_paper_title,
+            date: req.body.date,
+            time_from: req.body.time_from,
+            time_to: req.body.time_to,
+            status: status,
+        };
+        mongo.connect(url, function(err, db) {
+            autoIncrement.getNextSequence(db, 'exams', function(err, autoIndex) {
+                var collection = db.collection('exams');
+                collection.ensureIndex({
+                    "exam_paper_id": 1,
+                }, {
+                    unique: true
+                }, function(err, result) {
+                    if (item.subject_id == null || item.exam_sch_id == null || item.exam_paper_title == null || item.date == null) {
+                        res.end('null');
+                    } else {
+                        collection.insertOne(item, function(err, result) {
+                            if (err) {
+                                if (err.code == 11000) {
+                                    res.end('false');
+                                }
+                                res.end('false');
+                            }
+                            collection.update({
+                                _id: item._id
+                            }, {
+                                $set: {
+                                    exam_paper_id: exam_sch_id+'-'+subject_id+'-EXM-'+autoIndex
+                                }
+                            }, function(err, result) {
+                                db.close();
+                                res.end('true');
+                            });
+                        });
+                    }
+                });
+            });
+        });
+
+    })
+    .get(function(req, res, next) {
+      var subject_id = req.params.subject_id;
+      var exam_sch_id = req.params.exam_sch_id;
+        var resultArray = [];
+        mongo.connect(url, function(err, db) {
+            assert.equal(null, err);
+            var cursor = db.collection('exams').find({subject_id, exam_sch_id});
+            cursor.forEach(function(doc, err) {
+                assert.equal(null, err);
+                resultArray.push(doc);
+            }, function() {
+                db.close();
+                res.send({
+                    [exam_sch_id+'-'+subject_id]: resultArray
+                });
+            });
+        });
+    });
+
+    router.route('/get_exam/:exam_paper_id')
+    .get(function(req, res, next) {
+      var exam_paper_id = req.params.exam_paper_id;
+        var resultArray = [];
+        mongo.connect(url, function(err, db) {
+            assert.equal(null, err);
+            var cursor = db.collection('exams').find({exam_paper_id});
+            cursor.forEach(function(doc, err) {
+                assert.equal(null, err);
+                resultArray.push(doc);
+            }, function() {
+                db.close();
+                res.send({
+                    exam: resultArray
+                });
+            });
+        });
+    });
+
+    router.route('/exam_edit/:exam_paper_id')
+        .post(function(req, res, next){
+          var exam_paper_id = req.params.exam_paper_id;
+          var name = req.body.name;
+          var value = req.body.value;
+          if (name == 'status') {
+            var value = parseInt(req.body.value);
+          }
+
+          mongo.connect(url, function(err, db){
+                db.collection('exams').update({exam_paper_id},{$set:{[name]: value}}, function(err, result){
+                  assert.equal(null, err);
+                   db.close();
+                   res.send('true');
+                });
+          });
+        });
+
+
+    router.route('/exam_eval/:exam_paper_id/:student_id')
+        .post(function(req, res, next) {
+            var status = 1;
+            var exam_paper_id = req.params.exam_paper_id;
+            var student_id = req.params.student_id;
+            var date = new Date();
+            console.log(date);
+            subjects = [];
+            var item = {
+                paper_result_id: 'getauto',
+                exam_paper_id: exam_paper_id,
+                student_id: student_id,
+                marks: req.body.marks,
+                comment: req.body.comment,
+                date: date,
+                status: status,
+            };
+            mongo.connect(url, function(err, db) {
+                autoIncrement.getNextSequence(db, 'exam_evaluation', function(err, autoIndex) {
+                  var count = db.collection('exam_evaluation').find({ $and: [{exam_paper_id, student_id}]}).count(function (e, count){
+                    if (count > 0) {
+                      db.close();
+                      res.end('already submitted');
+                    }
+                  });
+                    var collection = db.collection('exam_evaluation');
+                    collection.ensureIndex({
+                        "paper_result_id": 1,
+                    }, {
+                        unique: true
+                    }, function(err, result) {
+                        if (item.exam_paper_id == null || item.student_id == null || item.marks == null || item.comment == null) {
+                            res.end('null');
+                        } else {
+                            collection.insertOne(item, function(err, result) {
+                                if (err) {
+                                    if (err.code == 11000) {
+                                        res.end('false');
+                                    }
+                                    res.end('false');
+                                }
+                                collection.update({
+                                    _id: item._id
+                                }, {
+                                    $set: {
+                                        paper_result_id: exam_paper_id+'-'+student_id+'-EVAL-'+autoIndex
+                                    }
+                                }, function(err, result) {
+                                    db.close();
+                                    res.end('true');
+                                });
+                            });
+                        }
+                    });
+                });
+            });
+
+        })
+        .get(function(req, res, next) {
+          var exam_paper_id = req.params.exam_paper_id;
+          var student_id = req.params.student_id;
+            var resultArray = [];
+            mongo.connect(url, function(err, db) {
+                assert.equal(null, err);
+                var cursor = db.collection('exam_evaluation').find({exam_paper_id, student_id});
+                cursor.forEach(function(doc, err) {
+                    assert.equal(null, err);
+                    resultArray.push(doc);
+                }, function() {
+                    db.close();
+                    res.send({
+                        [exam_paper_id+'-'+subject_id]: resultArray
+                    });
+                });
+            });
+        });
+
+      // router.route('/chk_exam_eval/:exam_paper_id/:student_id')
+      // .get(function(req, res, next) {
+      //   var exam_paper_id = req.params.exam_paper_id;
+      //   var student_id = req.params.student_id;
+      //     var resultArray = [];
+      //     mongo.connect(url, function(err, db) {
+      //         assert.equal(null, err);
+      //         var count = db.collection('exam_evaluation').find({exam_paper_id, student_id}).count();
+      //         cursor.forEach(function(doc, err) {
+      //             assert.equal(null, err);
+      //             resultArray.push(doc);
+      //         }, function() {
+      //             db.close();
+      //             res.send({
+      //                 count: resultArray
+      //             });
+      //         });
+      //     });
+      // });
+
+
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,104 @@
+// flow
+var schools = require("./api_components/schools.js");
+var exam_schedule = require("./api_components/exam_schedule.js");
+var exams = require("./api_components/exams.js");
+var school_classes = require("./api_components/school_classes.js");
+var students = require("./api_components/students.js");
+var subjects = require("./api_components/subject.js");
+var course_works = require("./api_components/course_work.js");
+var attendance = require("./api_components/attendance.js");
+var employee = require("./api_components/employee.js");
+var staff_user = require("./api_components/staff_user.js");
+var teachers = require("./api_components/teacher.js");
+var assesment = require("./api_components/assesment.js");
+const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt',{ session: false });
+const requireSignin = passport.authenticate('local',{session: false});
+
+var config = require("./config.json");
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var server = require('http').createServer(app);
+var api_key = "api-key-KJFSI4924R23RFSDFSD7F94";
+var mongo = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+var port = process.env.PORT || 4005;
+var router = express.Router();
+var fs = require("fs");
+var url = 'mongodb://' +config.dbhost + ':27017/s_erp_data';
+// DB Setup for Auth
+mongoose.connect('mongodb://localhost:auth/auth');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+router.use(function(req, res, next) {
+    // do logging
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+app.use(function(req, res, next) {
+    // do logging
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+app.get('/', function (req, res) {
+  res.send('School ERP API');
+});
+
+
+app.get('/secure', requireAuth, function (req, res) {
+  res.send('School ERP API -  Authorised Page');
+});
+
+app.post('/signin', requireSignin, Authentication.signin);
+
+app.post('/signup', Authentication.signup);
+
+router.route('/em')
+    .get(function(req, res, next) {
+        mongo.connect(url, function(err, db) {
+            if (!err) {
+                res.send("Hay! We have a connection WOW Great");
+                return true;
+            } else {
+                console.log(err);
+                res.send("I think you have done something wrong!!");
+            }
+        });
+
+    });
+
+
+app.listen(port, function(){
+   console.log('Express server listening on port ' + port);
+});
+
+app.use('/api', schools);
+app.use('/api', exams);
+app.use('/api', exam_schedule);
+app.use('/api', school_classes);
+app.use('/api', teachers);
+app.use('/api', students);
+app.use('/api', subjects);
+app.use('/api', course_works);
+app.use('/api', attendance);
+app.use('/api', employee);
+app.use('/api', staff_user);
+app.use('/api', assesment);
+app.use('/api', router);
