c1e8c9d59c6cafec9c83890d41930201df440273
Jagadeesh <ajagadeesh@gleedtech.com>
2017-07-04 18:26:51 +0530

    code update

--- api_components/schoolevent.js
+++ api_components/schoolevent.js
@@ -18,9 +18,8 @@ router.use(function(req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
-// Add Schools
-
-router.route('/timetable/:school_events')
+// Add School Events
+router.route('/schoolevents/:school_id')
     .post(function(req, res, next) {
         var status = 1;
         var school_id = req.params.school_id;
@@ -32,10 +31,11 @@ router.route('/timetable/:school_events')
             date:req.body.date,
             time:req.body.time,
             description:req.body.description,
+            status: status
 
         };
         mongo.connect(url, function(err, db) {
-            autoIncrement.getNextSequence(db, function(err, autoIndex) {
+            autoIncrement.getNextSequence(db,'schoolevents', function(err, autoIndex) {
                 var collection = db.collection('schoolevents');
                 collection.ensureIndex({
                     "school_event_id": 1,
@@ -54,10 +54,10 @@ router.route('/timetable/:school_events')
                                 res.end('false');
                             }
                             collection.update({
-                                school_events_id: item._id
+                                 _id: item._id
                             }, {
                                 $set: {
-                                    _id: 'SCHOOL_EVENT-'+autoIndex
+                                    school_event_id: 'SCHOOL_EVENT-'+autoIndex
                                 }
                             }, function(err, result) {
                                 db.close();
@@ -71,14 +71,12 @@ router.route('/timetable/:school_events')
     })
     .get(function(req, res, next) {
         var resultArray = [];
-            event_title= req.params.event_title,
-            date=req.params.date,
-            time=req.params.time,
-            description=req.params.description,
+         var school_id = req.params.school_id;
+         
 
         mongo.connect(url, function(err, db) {
             assert.equal(null, err);
-            var cursor = db.collection('schoolevents').find({event_title,date,time,description});
+            var cursor = db.collection('schoolevents').find({school_id});
             cursor.forEach(function(doc, err) {
                 assert.equal(null, err);
                 resultArray.push(doc);
